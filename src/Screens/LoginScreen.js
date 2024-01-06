96f930a2e745d2fc3c44e2b0fc24c59f315840b4
Andrew <a.nikanorov@gmail.com>
2024-01-06 14:35:52 +0100

    add TouchableWithoutFeedback

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -7,8 +7,10 @@ import {
 	View,
 	ImageBackground,
 	TextInput,
+	Keyboard,
 	KeyboardAvoidingView,
 	TouchableOpacity,
+	TouchableWithoutFeedback,
 	Alert,
 } from 'react-native';
 import { useFonts } from 'expo-font';
@@ -56,58 +58,60 @@ function RegistrationScreen({ navigation }) {
 	}
 
 	return (
-		<View style={styles.container}>
-			<ImageBackground source={LogoImage} style={styles.image}>
-				<KeyboardAvoidingView
-					behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-					style={styles.form}
-				>
-					<Text style={styles.title}>Sign Into Your Account</Text>
-					<View style={styles.inputBox}>
-						<TextInput
-							value={email}
-							onChangeText={setEmail}
-							placeholder='Email'
-							style={[styles.input, isFocusedEmail && styles.focusedInput]}
-							onFocus={() => handleFocus('email')}
-						/>
-						<View style={styles.inputPassContainer}>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View style={styles.container}>
+				<ImageBackground source={LogoImage} style={styles.image}>
+					<KeyboardAvoidingView
+						behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+						style={styles.form}
+					>
+						<Text style={styles.title}>Sign Into Your Account</Text>
+						<View style={styles.inputBox}>
 							<TextInput
-								value={password}
-								onChangeText={setPassword}
-								placeholder='Password'
-								secureTextEntry={!showPassword}
-								style={[styles.input, isFocusedPassword && styles.focusedInput]}
-								onFocus={() => handleFocus('password')}
+								value={email}
+								onChangeText={setEmail}
+								placeholder='Email'
+								style={[styles.input, isFocusedEmail && styles.focusedInput]}
+								onFocus={() => handleFocus('email')}
 							/>
+							<View style={styles.inputPassContainer}>
+								<TextInput
+									value={password}
+									onChangeText={setPassword}
+									placeholder='Password'
+									secureTextEntry={!showPassword}
+									style={[styles.input, isFocusedPassword && styles.focusedInput]}
+									onFocus={() => handleFocus('password')}
+								/>
+								<TouchableOpacity
+									onPress={toggleShowPassword}
+									style={styles.showPasswordButton}
+								>
+									<Text style={styles.showPasswordText}>
+										{showPassword ? 'Hide' : 'Show'}{' '}
+									</Text>
+								</TouchableOpacity>
+							</View>
+						</View>
+						<View style={styles.buttonBox}>
+							<TouchableOpacity style={styles.button} onPress={onLogin}>
+								<Text style={styles.buttonText}>Login</Text>
+							</TouchableOpacity>
 							<TouchableOpacity
-								onPress={toggleShowPassword}
-								style={styles.showPasswordButton}
+								style={styles.singupLink}
+								onPress={() => navigation.navigate('Registration')}
 							>
-								<Text style={styles.showPasswordText}>
-									{showPassword ? 'Hide' : 'Show'}{' '}
+								<Text style={styles.signup}>
+									Don't have an account?{' '}
+									<Text style={{ textDecorationLine: 'underline' }}>Sign Up</Text>
 								</Text>
 							</TouchableOpacity>
 						</View>
-					</View>
-					<View style={styles.buttonBox}>
-						<TouchableOpacity style={styles.button} onPress={onLogin}>
-							<Text style={styles.buttonText}>Login</Text>
-						</TouchableOpacity>
-						<TouchableOpacity
-							style={styles.singupLink}
-							onPress={() => navigation.navigate('Registration')}
-						>
-							<Text style={styles.signup}>
-								Don't have an account?{' '}
-								<Text style={{ textDecorationLine: 'underline' }}>Sign Up</Text>
-							</Text>
-						</TouchableOpacity>
-					</View>
-				</KeyboardAvoidingView>
-			</ImageBackground>
-			<StatusBar style='auto' />
-		</View>
+					</KeyboardAvoidingView>
+				</ImageBackground>
+				<StatusBar style='auto' />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
--- src/Screens/RegistrationScreen.js
+++ src/Screens/RegistrationScreen.js
@@ -7,9 +7,10 @@ import {
 	View,
 	ImageBackground,
 	TextInput,
+	Keyboard,
 	KeyboardAvoidingView,
 	TouchableOpacity,
-	Image,
+	TouchableWithoutFeedback,
 	Alert,
 } from 'react-native';
 import { useFonts } from 'expo-font';
@@ -68,71 +69,73 @@ function LoginScreen({ navigation }) {
 	}
 
 	return (
-		<View style={styles.container}>
-			<ImageBackground source={LogoImage} style={styles.image}>
-				<KeyboardAvoidingView
-					behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-					style={styles.formReg}
-				>
-					<View style={styles.avatarContainer}>
-						<View style={styles.addAvatar}>
-							<AddIcon />
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View style={styles.container}>
+				<ImageBackground source={LogoImage} style={styles.image}>
+					<KeyboardAvoidingView
+						behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+						style={styles.formReg}
+					>
+						<View style={styles.avatarContainer}>
+							<View style={styles.addAvatar}>
+								<AddIcon />
+							</View>
 						</View>
-					</View>
 
-					<Text style={styles.title}>Create an Account</Text>
-					<View style={styles.inputBox}>
-						<TextInput
-							value={login}
-							onChangeText={setLogin}
-							placeholder='Login'
-							style={[styles.input, isFocusedLogin && styles.focusedInput]}
-							onFocus={() => handleFocus('login')}
-						/>
-						<TextInput
-							value={email}
-							onChangeText={setEmail}
-							placeholder='Email'
-							style={[styles.input, isFocusedEmail && styles.focusedInput]}
-							onFocus={() => handleFocus('email')}
-						/>
-						<View style={styles.inputPassContainer}>
+						<Text style={styles.title}>Create an Account</Text>
+						<View style={styles.inputBox}>
 							<TextInput
-								value={password}
-								onChangeText={setPassword}
-								placeholder='Password'
-								secureTextEntry={!showPassword}
-								style={[styles.input, isFocusedPassword && styles.focusedInput]}
-								onFocus={() => handleFocus('password')}
+								value={login}
+								onChangeText={setLogin}
+								placeholder='Login'
+								style={[styles.input, isFocusedLogin && styles.focusedInput]}
+								onFocus={() => handleFocus('login')}
 							/>
+							<TextInput
+								value={email}
+								onChangeText={setEmail}
+								placeholder='Email'
+								style={[styles.input, isFocusedEmail && styles.focusedInput]}
+								onFocus={() => handleFocus('email')}
+							/>
+							<View style={styles.inputPassContainer}>
+								<TextInput
+									value={password}
+									onChangeText={setPassword}
+									placeholder='Password'
+									secureTextEntry={!showPassword}
+									style={[styles.input, isFocusedPassword && styles.focusedInput]}
+									onFocus={() => handleFocus('password')}
+								/>
+								<TouchableOpacity
+									onPress={toggleShowPassword}
+									style={styles.showPasswordButton}
+								>
+									<Text style={styles.showPasswordText}>
+										{showPassword ? 'Hide' : 'Show'}{' '}
+									</Text>
+								</TouchableOpacity>
+							</View>
+						</View>
+						<View style={styles.buttonBox}>
+							<TouchableOpacity style={styles.button} onPress={onLogin}>
+								<Text style={styles.buttonText}>Sign Up</Text>
+							</TouchableOpacity>
 							<TouchableOpacity
-								onPress={toggleShowPassword}
-								style={styles.showPasswordButton}
+								style={styles.singupLink}
+								onPress={() => navigation.navigate('Login')}
 							>
-								<Text style={styles.showPasswordText}>
-									{showPassword ? 'Hide' : 'Show'}{' '}
+								<Text style={styles.signup}>
+									Do have you an account?{' '}
+									<Text style={{ textDecorationLine: 'underline' }}>Sign In</Text>
 								</Text>
 							</TouchableOpacity>
 						</View>
-					</View>
-					<View style={styles.buttonBox}>
-						<TouchableOpacity style={styles.button} onPress={onLogin}>
-							<Text style={styles.buttonText}>Sign Up</Text>
-						</TouchableOpacity>
-						<TouchableOpacity
-							style={styles.singupLink}
-							onPress={() => navigation.navigate('Login')}
-						>
-							<Text style={styles.signup}>
-								Do have you an account?{' '}
-								<Text style={{ textDecorationLine: 'underline' }}>Sign In</Text>
-							</Text>
-						</TouchableOpacity>
-					</View>
-				</KeyboardAvoidingView>
-			</ImageBackground>
-			<StatusBar style='auto' />
-		</View>
+					</KeyboardAvoidingView>
+				</ImageBackground>
+				<StatusBar style='auto' />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
